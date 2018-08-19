c9aa01024f397553f87afabffd6aa659b10337aa
Justin Rokisky <jrokisky@mindgrub.com>
2018-08-19 19:13:26 -0400

    Added launch file & rotation between range

--- src/joint_controller.cpp
+++ src/joint_controller.cpp
@@ -1,36 +1,54 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <dynamixel_workbench_msgs/JointCommand.h>
+#include <string>
+#include <cmath>
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "joint_controller");
-  ros::NodeHandle nh;
+  ros::NodeHandle nh("joint_controller");
 
   ros::ServiceClient joint_command_client = 
-	  nh.serviceClient<dynamixel_workbench_msgs::JointCommand>("joint_command");
+	  nh.serviceClient<dynamixel_workbench_msgs::JointCommand>("/joint_command");
+  joint_command_client.waitForExistence();
  
-  float goal_position = 5.0;
+  float goal_position = 0.0;
+
+  // Dynamixel unit.
+  std::string unit = nh.param<std::string>("unit", "rad");
+  // Rotation increment.
+  float goal_pos_inc = nh.param<float>("goal_pos_inc", 0.0);
+  // Maximum rotation.
+  float max_rad = nh.param<float>("max_rad", 0.0);
 
   ros::Rate r(1);
   while (ros::ok()) 
   {
     dynamixel_workbench_msgs::JointCommand joint_command;
-    joint_command.request.unit = "AX-12A"; //TODO: make param. 
+    joint_command.request.unit = unit;
     joint_command.request.id = 1;
     joint_command.request.goal_position = goal_position;
 
     if (joint_command_client.call(joint_command))
     {
       if (!joint_command.response.result)
-        ROS_INFO("Failed to write goal_position");
+        ROS_INFO("Failed to write goal_position: %f", goal_position);
+      else
+        ROS_INFO("Wrote goal_position: %f", goal_position);
     }
     else
     {
       ROS_INFO("Failed to call service");
     }
 
-    goal_position += 5.0;
+    // Head in the other direction.
+    float tmp = goal_position + goal_pos_inc;
+    if (std::abs(tmp) > max_rad) 
+      goal_pos_inc *= -1.0;
+
+    goal_position += goal_pos_inc;
     r.sleep();
     ros::spinOnce();
   }
 }
+
